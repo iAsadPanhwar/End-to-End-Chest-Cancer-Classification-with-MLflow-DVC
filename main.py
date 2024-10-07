4d422fecebeb6118c3546041daa495140ce416d3
Asad Ali Panhwar <asadalipuh5@gmail.com>
2024-10-07 12:34:53 +0500

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from src.cnnCLassifier import logger
+from cnnCLassifier import logger
+from cnnCLassifier.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to cnnClassifier")
+
+STAGE_NAME = "Data Ingestion Step"
+
+try:
+    logger.info(f'>>>>>>>> stage {STAGE_NAME} started <<<<<<<<')
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\n X===========X')
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/cnnCLassifier/utils/common.py
+++ src/cnnCLassifier/utils/common.py
@@ -2,7 +2,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from cnnClassifier import logger
+from cnnCLassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
