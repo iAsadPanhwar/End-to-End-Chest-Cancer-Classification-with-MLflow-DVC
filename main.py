b3378c381f3331514c756c71120d6e76534dc908
Asad Ali Panhwar <asadalipuh5@gmail.com>
2024-10-08 05:11:33 +0500

    Model Trainer Updated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cnnCLassifier import logger
 from cnnCLassifier.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnCLassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnCLassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Step"
 
@@ -22,6 +24,19 @@ try:
     obj.main()
     logger.info(f'>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\n X========X')
         
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f'******************************')
+    logger.info(f'>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<')
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx=================x')
+    
 except Exception as e:
     logger.exception(e)
     raise e
--- src/cnnCLassifier/config/configuration.py
+++ src/cnnCLassifier/config/configuration.py
@@ -1,7 +1,9 @@
+import os
 from cnnCLassifier.constants import *
 from cnnCLassifier.utils.common import read_yaml, create_directories
 from cnnCLassifier.entity.entity_config import (DataIngestionConfig,
-                                                PrepareBaseModelConfig)
+                                                PrepareBaseModelConfig,
+                                                TrainingConfig)
 
 
 class ConfigurationManager:
@@ -41,7 +43,29 @@ class ConfigurationManager:
             params_image_size=self.params.IMAGE_SIZE,
             params_learning_rate=self.params.LEARNING_RATE,
             params_include_top=self.params.INCLUDE_TOP,
-            params_wights=self.params.WEIGHTS,
+            params_weights=self.params.WEIGHTS,
             params_classes=self.params.CLASSES
         )
         return prepare_base_model_config
+    
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chest-CT-Scan-data")
+        create_directories([
+            Path(training.root_dir)
+        ])
+        
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+        
+        return training_config
